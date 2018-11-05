fcb5cdfd5ec79c3d08ef9816092a5c4bff44b4c8
robyng442 <robjgraham@gmail.com>
2018-11-05 10:08:57 +0000

    Task 1 completed

--- src/apr/Main.java
+++ src/apr/Main.java
@@ -2,7 +2,6 @@ package apr;
 
 public class Main {
     public static void main(String[] args) {
-        AlphabetPositionReplacement apr = new AlphabetPositionReplacement();
-        System.out.println(apr.replacer("This NETbuilder assessment is way too easy."));
+        System.out.println(AlphabetPositionReplacement.replacer("This NETbuilder assessment is way too easy."));
     }
 }
